750ca5383b68e78cc910da4d6c33157372623945
harshkumarchourasia <harshkumarchourasia@gmail.com>
2025-02-17 20:18:29 +0530

    delete helper files to reduce size of project

--- day13/prereq/.ppc/grader.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from ppcgrader.cli import cli
-import ppcprereq
-
-if __name__ == "__main__":
-    cli(ppcprereq.Config(code='prereq'))
--- day13/prereq/.ppc/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cat > /box/prereq.cc
-chmod a-w /box/prereq.cc
-
-cd /program
-/program/.ppc/grader.py --file "/box/prereq.cc" --binary "/box/prereq" --json "$@"
